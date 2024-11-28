3fbaec51ab8e4be8a9ab0b7b20488dc0cb7ab17a
Felipe Fontes <116237840+felipefontesoficial@users.noreply.github.com>
2024-11-28 00:04:27 -0300

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,1 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    layout="wide",
+    page_title="App Mivus"
+)
+
+
+logo = "caminho_para_sua_logo.png" 
+st.sidebar.image(logo, use_column_width=True)
+
+st.title("Olá, Querido Lendário 😀")
+st.write("Agora, tudo o que você precisa estará organizado em um só lugar.")
+
 
