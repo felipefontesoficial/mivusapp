239070f2b1d21a1df22beb28e6dc4f05c82300e9
Felipe Fontes <116237840+felipefontesoficial@users.noreply.github.com>
2025-01-22 00:34:50 -0300

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -17,28 +17,14 @@ st.markdown("---") # Separador
 
 #Título da Coluna
 
-col1, col2 = st.columns([0.7, 0.3])
-col1.subheader(":balloon: Desafio 70 Leads em 1 Semana ")
-col2.write("**Data:** 02/12 à 06/12/2024")
+#col1, col2 = st.columns([0.7, 0.3])
+#col1.subheader(":balloon: Desafio 70 Leads em 1 Semana ")
+#col2.write("**Data:** 02/12 à 06/12/2024")
 
-st.write("\n") #Espaço em branco
+#st.write("\n") #Espaço em branco
 
 
 # Apresentação do desafio
 
-st.markdown("""**Regras do Desafio:**
+#st.markdown("""**Regras do Desafio:**
 
-Você deverá prospectar 70 Clientes em 1 Semana.
-
-Todos os dias, ao final do dia ( 17:30h ) você deverá mostrar sua comprovação de abordagens diárias. ( *Tire print do relatório do seu CRM e mande no grupo* )
-
-**Recomendação:** Faça 15 abordagens por dia, e na sexta faça apenas 10. 
-
-*Conta como abordagem:* Ligação, E-mail, Mensagem do WhatsApp. Seja primeiro contato ou Follow Up. 
-
-**Premiação:**
-
-- **Aula** Ao Vivo Exclusiva Sobre Copy
-- **Gravação** de Ligações de Prospecção da Mivus
-- **Bastidores** de Uma Venda ( Analisando Copy )
-- **Acesso** ao Novo Roteiro de Prospecção Mivus""")
